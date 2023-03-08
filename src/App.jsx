99a730e0eac13ce5066bcc8c53b3ad71a1cad823
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2023-03-08 01:03:34 -0600

    Init

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [state, setState] = useState("");
+  const [position, setPosition] = useState("");
+  const [salary, setSalary] = useState(0);
+
+  const displayInfo = () => {
+    console.log(name + age + state + position + salary);
+  };
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Name</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <label>Age</label>
+        <input
+          type="number"
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
+        <label>State</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setState(e.target.value);
+          }}
+        />
+        <label>Position</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setPosition(e.target.value);
+          }}
+        />
+        <label>Salary</label>
+        <input
+          type="number"
+          onChange={(e) => {
+            setSalary(e.target.value);
+          }}
+        />
+        <button onClick={displayInfo}>Add Employee</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
